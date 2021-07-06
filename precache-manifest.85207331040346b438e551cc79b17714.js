33dae4aef4108fd544a7742b1a87a24c5b7e6e3d
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-07-06 16:31:37 +0200

    Updates

---
+++ precache-manifest.85207331040346b438e551cc79b17714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189eb3ea34f5cb835e785748f72bcf0c",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "806f92506e30c057efcd",
+    "url": "/cv/static/css/main.d7174023.chunk.css"
+  },
+  {
+    "revision": "00e6797a921cf5b67134",
+    "url": "/cv/static/js/2.adb2ee4e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cv/static/js/2.adb2ee4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "806f92506e30c057efcd",
+    "url": "/cv/static/js/main.05e48a13.chunk.js"
+  },
+  {
+    "revision": "4157253bc5d6dffe1da9",
+    "url": "/cv/static/js/runtime-main.3c78338b.js"
+  },
+  {
+    "revision": "273645ed7a701aa1165a1ca2396182c1",
+    "url": "/cv/static/media/33637358.273645ed.jfif"
+  }
+]);
